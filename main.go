edae98385017d60fe5646c00e12a9e5668d485ac
andrey <andereishuk@gmail.com>
2026-03-15 12:57:59 +0300

    добавление подключения к DB

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"study/feature"
 	"study/feature2"
+	"study/postgres"
 )
 
 func main() {
 	fmt.Println("hello git")
 	feature.Feature()
 	feature2.Feature2()
+	postgres.ConnetctionToPostgres()
 }
---
+++ postgres/postgres.go
@@ -0,0 +1,22 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func ConnetctionToPostgres() {
+	ctx := context.Background()
+	connect, err := pgx.Connect(ctx, "postgres://postgres:postgres@localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := connect.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Успешное подключение")
+}
